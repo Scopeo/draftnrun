4e523b82b0212ce23797fa4e505678d131b867a7
dianascopeo <diana@draftnrun.com>
2025-06-18 15:54:30 +0200

    fix tests and libr issue

--- tests/data_ingestion/test_ingestion_document.py
+++ tests/data_ingestion/test_ingestion_document.py
@@ -7,17 +7,21 @@ def test_list_folder_documents():
     folder_manager = LocalFolderManager(path)
     documents = folder_manager.list_all_files_info()
     assert documents is not None
-    assert len(documents) == 3
+    assert len(documents) == 4
     documents = sorted(documents, key=lambda x: x.id)
-    assert documents[0].id == "tests/resources/documents/test_docx.docx"
-    assert documents[0].type == FileDocumentType.DOCX
-    assert documents[0].file_name == "test_docx.docx"
-    assert documents[0].folder_name == "tests/resources/documents"
-    assert documents[1].id == "tests/resources/documents/test_markdown.md"
-    assert documents[1].type == FileDocumentType.MARKDOWN
-    assert documents[1].file_name == "test_markdown.md"
+    print(documents)
+    assert documents[0].id == "tests/resources/documents/sample.pdf"
+    assert documents[0].type == FileDocumentType.PDF
+    assert documents[0].file_name == "sample.pdf"
+    assert documents[1].id == "tests/resources/documents/test_docx.docx"
+    assert documents[1].type == FileDocumentType.DOCX
+    assert documents[1].file_name == "test_docx.docx"
     assert documents[1].folder_name == "tests/resources/documents"
-    assert documents[2].id == "tests/resources/documents/test_pdf.pdf"
-    assert documents[2].type == FileDocumentType.PDF
-    assert documents[2].file_name == "test_pdf.pdf"
+    assert documents[2].id == "tests/resources/documents/test_markdown.md"
+    assert documents[2].type == FileDocumentType.MARKDOWN
+    assert documents[2].file_name == "test_markdown.md"
     assert documents[2].folder_name == "tests/resources/documents"
+    assert documents[3].id == "tests/resources/documents/test_pdf.pdf"
+    assert documents[3].type == FileDocumentType.PDF
+    assert documents[3].file_name == "test_pdf.pdf"
+    assert documents[3].folder_name == "tests/resources/documents"
