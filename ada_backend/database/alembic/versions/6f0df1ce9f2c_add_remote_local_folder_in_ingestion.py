e639189e47c0ed26fe7e4297570b6515f6270ed3
jccscopeo <jean-christophe@draftnrun.com>
2025-06-27 19:39:55 +0200

    Update the credentials on the CI + Check if bucket exists or not + adapt docker compose to solve seaweedfs restart issue

---
+++ ada_backend/database/alembic/versions/6f0df1ce9f2c_add_remote_local_folder_in_ingestion.py
@@ -0,0 +1,40 @@
+"""add_remote_local_folder_in_ingestion
+
+Revision ID: 6f0df1ce9f2c
+Revises: 80118747a315
+Create Date: 2025-06-27 17:14:28.114990
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "6f0df1ce9f2c"
+down_revision: Union[str, None] = "80118747a315"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    # Add the new value to the enum
+    op.execute("ALTER TYPE source_type ADD VALUE IF NOT EXISTS 'remote_local'")
+
+
+def downgrade() -> None:
+    op.execute(
+        """
+        DELETE FROM ingestion_tasks
+        WHERE source_id IN (
+            SELECT id FROM data_sources
+            WHERE type = 'remote_local'
+        )
+    """
+    )
+    op.execute(
+        """
+        DELETE FROM data_sources
+        WHERE type = 'remote_local'
+    """
+    )
