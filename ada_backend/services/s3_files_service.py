a9f4887fe34a6650c5ab24341aa747a6989deef0
Jean-Christophe Corvisier <107189926+jccscopeo@users.noreply.github.com>
2025-07-11 15:37:14 +0200

    feat(BugFix Ingestion):Add bugfix to put the s3 bucket initialization lazy (#64)
    * feat(BugFix Ingestion):Add bugfix to put the s3 bucket initialization as lazy and during the call of s3 endpoint via cached function

--- ada_backend/services/s3_files_service.py
+++ ada_backend/services/s3_files_service.py
@@ -1,4 +1,7 @@
 import logging
+from functools import lru_cache
+
+import boto3
 
 from ada_backend.schemas.ingestion_task_schema import (
     S3UploadedInformation,
@@ -15,10 +18,24 @@ from settings import settings
 
 LOGGER = logging.getLogger(__name__)
 
-S3_CLIENT = get_s3_boto3_client()
-if not is_bucket_existing(s3_client=S3_CLIENT, bucket_name=settings.S3_BUCKET_NAME):
-    LOGGER.warning(f"Bucket {settings.S3_BUCKET_NAME} does not exist.Creating it.")
-    create_bucket(s3_client=S3_CLIENT, bucket_name=settings.S3_BUCKET_NAME)
+
+@lru_cache()
+def get_s3_client_and_ensure_bucket() -> boto3.client:
+    """
+    Lazily create the S3 client and ensure the bucket exists.
+    """
+    s3_client = get_s3_boto3_client()
+    if settings.S3_BUCKET_NAME is None:
+        raise ValueError(
+            "S3_BUCKET_NAME (bucket to store files for ingestion) is not configured in settings."
+            " Please set it in the credentials.env file."
+        )
+
+    if not is_bucket_existing(s3_client=s3_client, bucket_name=settings.S3_BUCKET_NAME):
+        LOGGER.warning(f"Bucket {settings.S3_BUCKET_NAME} does not exist. Creating it.")
+        create_bucket(s3_client=s3_client, bucket_name=settings.S3_BUCKET_NAME)
+
+    return s3_client
 
 
 def upload_file_to_s3(
@@ -29,8 +46,9 @@ def upload_file_to_s3(
     """Upload a file to an S3 bucket."""
     sanitized_key = sanitize_filename(file_name, remove_extension_dot=False)
     try:
+        s3_client = get_s3_client_and_ensure_bucket()
         upload_file_to_bucket(
-            s3_client=S3_CLIENT, bucket_name=bucket_name, key=sanitized_key, byte_content=byte_content
+            s3_client=s3_client, bucket_name=bucket_name, key=sanitized_key, byte_content=byte_content
         )
         LOGGER.info(f"Successfully uploaded file to S3 with {sanitized_key} key.")
         return S3UploadedInformation(s3_path_file=sanitized_key)
@@ -44,7 +62,8 @@ def delete_file_from_s3(
     bucket_name: str = settings.S3_BUCKET_NAME,
 ) -> None:
     try:
-        delete_file_from_bucket(s3_client=S3_CLIENT, bucket_name=bucket_name, key=key)
+        s3_client = get_s3_client_and_ensure_bucket()
+        delete_file_from_bucket(s3_client=s3_client, bucket_name=bucket_name, key=key)
         LOGGER.info(f"Successfully deleted file from S3 with {key} key.")
     except Exception as e:
         LOGGER.error(f"Error deleting file from S3: {str(e)}")
