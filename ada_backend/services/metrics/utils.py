46f90df9db2d8d2bd7354e25a44d30f5da59c832
natacha-scopeo <natacha@draftnrun.com>
2025-07-11 15:52:08 +0200

    feat(obs)/new messages table

--- ada_backend/services/metrics/utils.py
+++ ada_backend/services/metrics/utils.py
@@ -1,11 +1,14 @@
 from datetime import timedelta, datetime
 import json
 from uuid import UUID
+import logging
 
 import numpy as np
 import pandas as pd
 
 from engine.trace.sql_exporter import get_session_trace
+
+LOGGER = logging.getLogger(__name__)
 
 
 def query_trace_duration(project_id: UUID, duration_days: int) -> pd.DataFrame:
@@ -25,3 +28,24 @@ def query_trace_duration(project_id: UUID, duration_days: int) -> pd.DataFrame:
         "trace_rowid"
     ].values
     return df[df["trace_rowid"].isin(trace_rowids)]
+
+
+def query_trace_messages(
+    duration_days: int,
+) -> pd.DataFrame:
+    start_time_offset_days = (datetime.now() - timedelta(days=duration_days)).isoformat()
+    LOGGER.debug(f"Querying messages for since {start_time_offset_days}")
+
+    query = (
+        "SELECT m1.span_id, m1.content AS input_content, m2.content AS output_content "
+        "FROM messages m1 JOIN messages m2 ON m1.span_id = m2.span_id "
+        "JOIN spans s ON m1.span_id = s.span_id "
+        "WHERE m1.direction = 'input' AND m2.direction = 'output' "
+        f"AND s.start_time > '{start_time_offset_days}';"
+    )
+    session = get_session_trace()
+    df = pd.read_sql_query(query, session.bind)
+    session.close()
+    df = df.replace({np.nan: None})
+
+    return df
