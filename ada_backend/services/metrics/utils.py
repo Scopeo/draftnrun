c607efc6600a8dab95bab1cbc2bb24a8b057ba3e
natacha-scopeo <129970195+natacha-scopeo@users.noreply.github.com>
2025-08-01 16:53:56 +0200

    feat(obs)/ create span messages table AIT-282 (#87)

--- ada_backend/services/metrics/utils.py
+++ ada_backend/services/metrics/utils.py
@@ -1,11 +1,14 @@
 from datetime import timedelta, datetime
 import json
 from uuid import UUID
+import logging
 
 import numpy as np
 import pandas as pd
 
 from engine.trace.sql_exporter import get_session_trace
+
+LOGGER = logging.getLogger(__name__)
 
 
 def query_trace_duration(project_id: UUID, duration_days: int) -> pd.DataFrame:
@@ -25,3 +28,21 @@ def query_trace_duration(project_id: UUID, duration_days: int) -> pd.DataFrame:
         "trace_rowid"
     ].values
     return df[df["trace_rowid"].isin(trace_rowids)]
+
+
+def query_trace_messages(
+    duration_days: int,
+) -> pd.DataFrame:
+    start_time_offset_days = (datetime.now() - timedelta(days=duration_days)).isoformat()
+    LOGGER.debug(f"Querying messages for since {start_time_offset_days}")
+
+    query = (
+        "SELECT m.span_id, m.input_content, m.output_content FROM span_messages m "
+        "JOIN spans s ON m.span_id = s.span_id "
+        f"WHERE s.start_time > '{start_time_offset_days}';"
+    )
+    session = get_session_trace()
+    df = pd.read_sql_query(query, session.bind)
+    session.close()
+
+    return df
