32e7842bfd0da65cf034cbaa9998cb4327ede195
natacha-scopeo <natacha@draftnrun.com>
2025-08-01 13:15:09 +0200

    create 2 endpoints

--- ada_backend/routers/project_router.py
+++ ada_backend/routers/project_router.py
@@ -201,12 +201,29 @@ async def get_project_trace(
     project_id: UUID,
     duration: int,
     user: Annotated[SupabaseUser, Depends(get_user_from_supabase_token)],
-    include_messages: bool = False,
 ):
     if not user.id:
         raise HTTPException(status_code=400, detail="User ID not found")
     try:
-        response = get_trace_by_project(user.id, project_id, duration, include_messages)
+        response = get_trace_by_project(user.id, project_id, duration, include_messages=False)
+        return response
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e)) from e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error") from e
+
+
+# TODO: filter trace by graph_runner_id
+@router.get("/{project_id}/trace/v2", response_model=List[TraceSpan], tags=["Metrics"])
+async def get_project_trace(
+    project_id: UUID,
+    duration: int,
+    user: Annotated[SupabaseUser, Depends(get_user_from_supabase_token)],
+):
+    if not user.id:
+        raise HTTPException(status_code=400, detail="User ID not found")
+    try:
+        response = get_trace_by_project(user.id, project_id, duration, include_messages=True)
         return response
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e)) from e
