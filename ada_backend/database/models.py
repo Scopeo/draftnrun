91c8b1bd53efe4330a0fe1e5057c0bf8393d1160
dianascopeo <diana@draftnrun.com>
2025-08-08 15:49:32 +0200

    add env and call tyoe into trace attributes and trace endpoint

--- ada_backend/database/models.py
+++ ada_backend/database/models.py
@@ -74,6 +74,11 @@ class EnvType(StrEnum):
     PRODUCTION = "production"
 
 
+class CallType(StrEnum):
+    API = "api"
+    SANDBOX = "sandbox"
+
+
 class UIComponent(StrEnum):
     AUTOCOMPLETE = "Autocomplete"
     CHECKBOX = "Checkbox"
