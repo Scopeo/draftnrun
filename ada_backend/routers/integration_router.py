00608f1a8180838d67004fc05c9974fd39eed960
Pablo Skewes <pablo.skewes@ug.uchile.cl>
2025-09-03 23:09:39 -0400

    clean print

--- ada_backend/routers/integration_router.py
+++ ada_backend/routers/integration_router.py
@@ -54,7 +54,4 @@ async def add_integration_secrets(
             create_project_integration=create_project_integration,
         )
     except Exception as e:
-        import traceback
-
-        print(f"Error adding integration secrets: {traceback.format_exc()}")
         raise HTTPException(status_code=500, detail="Internal Server Error") from e
