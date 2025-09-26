9a1d35705af8539e34017b421ff2415ad36f1b32
natacha-scopeo <natacha@draftnrun.com>
2025-09-26 12:40:56 +0200

    fix alembic

--- ada_backend/database/alembic/versions/79e3872b3b03_add_agents.py
+++ ada_backend/database/alembic/versions/79e3872b3b03_add_agents.py
@@ -14,7 +14,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = "79e3872b3b03"
-down_revision: Union[str, None] = "c565c7904a64"
+down_revision: Union[str, None] = "44a8ee9bc189"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
