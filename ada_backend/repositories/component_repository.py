d9d266d7a0552758fd50b6e290749051d615f045
dianascopeo <119426960+dianascopeo@users.noreply.github.com>
2025-10-16 18:10:30 +0200

    feat(Bugfix): Not show llm api key DRA-557 (#204)

--- ada_backend/repositories/component_repository.py
+++ ada_backend/repositories/component_repository.py
@@ -276,6 +276,7 @@ def get_instance_parameters_with_definition(
             is_advanced=param_def.is_advanced,
         )
         for param, param_def in results
+        if param_def.type not in [db.ParameterType.LLM_API_KEY]  # Hide sensitive parameter types
     ]
 
 
