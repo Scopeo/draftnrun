ac5a54d5a2b1bc577ffac97e286a283b00d4c207
Pablo Skewes <pablo.skewes@ug.uchile.cl>
2025-11-07 01:17:54 -0300

    fix traces migrations

--- tests/alembic/test_trace_migrations.py
+++ tests/alembic/test_trace_migrations.py
@@ -5,6 +5,7 @@ pytestmark = pytest.mark.alembic
 from pytest_alembic.tests import test_single_head_revision
 from pytest_alembic.tests import test_upgrade
 from pytest_alembic.tests import test_up_down_consistency
+from pytest_alembic.tests import test_model_definitions_match_ddl
 
 from pytest_alembic.config import Config
 from pytest_mock_resources import create_postgres_fixture
