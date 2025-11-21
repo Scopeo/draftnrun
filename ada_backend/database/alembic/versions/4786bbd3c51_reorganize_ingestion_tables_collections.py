0123a6f3fdb4649ecd3f5cd4f6f2e6d9312b0cfa
natacha-scopeo <natacha@draftnrun.com>
2025-11-21 11:51:31 +0100

    fix alembic

--- ada_backend/database/alembic/versions/4786bbd3c51_reorganize_ingestion_tables_collections.py
+++ ada_backend/database/alembic/versions/4786bbd3c51_reorganize_ingestion_tables_collections.py
@@ -1,7 +1,7 @@
 """reorganize_ingestion_tables_collections
 
 Revision ID: 4786bbd3c51
-Revises: e2789dbf6d68
+Revises: b0ba5107a7e3
 Create Date: 2025-01-20 12:00:00.000000
 
 """
@@ -39,7 +39,7 @@ from engine.storage_service.local_service import SQLLocalService
 
 # revision identifiers, used by Alembic.
 revision: str = "4786bbd3c51"
-down_revision: Union[str, None] = "e2789dbf6d68"
+down_revision: Union[str, None] = "b0ba5107a7e3"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
