a275b21857617024ef7fedb75e1fb096bbd975a6
natacha-scopeo <natacha@draftnrun.com>
2025-12-09 18:56:08 +0100

    fix alembic

--- ada_backend/database/alembic/versions/62d077615136_add_graph_runner_modification_history.py
+++ ada_backend/database/alembic/versions/62d077615136_add_graph_runner_modification_history.py
@@ -1,7 +1,7 @@
 """add graph runner modification history
 
 Revision ID: 62d077615136
-Revises: a1b2c3d4e5f6
+Revises: 2b410fd2fb0d
 Create Date: 2025-12-04 14:58:12.773840
 
 """
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "62d077615136"
-down_revision: Union[str, None] = "a1b2c3d4e5f6"
+down_revision: Union[str, None] = "2b410fd2fb0d"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
