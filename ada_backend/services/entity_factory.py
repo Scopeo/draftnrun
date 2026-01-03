7e0dcba31166c4765541976238b36fc626970b95
Pablo Skewes <pablo.skewes@ug.uchile.cl>
2026-01-02 21:00:31 -0300

    fix

--- ada_backend/services/entity_factory.py
+++ ada_backend/services/entity_factory.py
@@ -21,7 +21,6 @@ from ada_backend.services.llm_models_service import (
     get_model_id_by_name_service,
 )
 from ada_backend.services.user_roles_service import get_user_access_to_organization
-<<<<<<< HEAD
 from engine.components.rag.cohere_reranker import CohereReranker
 from engine.components.rag.formatter import Formatter
 from engine.components.rag.retriever import Retriever
