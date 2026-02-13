5db3d44171965697fdc18c8ff3e89ec2814cf45b
natacha-scopeo <natacha@draftnrun.com>
2026-02-13 12:12:34 +0100

    fix revision

--- ada_backend/database/alembic/versions/a1b2c3d4e5f7_add_organization_id_to_qa_tables.py
+++ ada_backend/database/alembic/versions/a1b2c3d4e5f7_add_organization_id_to_qa_tables.py
@@ -1,7 +1,7 @@
 """add organization_id to qa tables
 
 Revision ID: a1b2c3d4e5f7
-Revises: b7e8f9a0c1d2
+Revises: cb22ac4f4a42
 Create Date: 2026-02-09 10:00:00.000000
 
 """
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "a1b2c3d4e5f7"
-down_revision: Union[str, None] = "b7e8f9a0c1d2"
+down_revision: Union[str, None] = "cb22ac4f4a42"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
