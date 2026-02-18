38df0866864605a76e0f288f2b8722e7b79607bc
dianascopeo <diana@draftnrun.com>
2026-02-18 11:36:35 +0100

    fix alembic

--- ada_backend/database/alembic/versions/a1b2c3d4e5f7_add_route_builder_ui_component.py
+++ ada_backend/database/alembic/versions/a1b2c3d4e5f7_add_route_builder_ui_component.py
@@ -1,7 +1,7 @@
 """add route builder ui component
 
 Revision ID: a1b2c3d4e5f7
-Revises: 67ec7c0706ec
+Revises: 7055cd620c33
 Create Date: 2026-02-06 12:20:00.000000
 
 """
@@ -12,7 +12,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "a1b2c3d4e5f7"
-down_revision: Union[str, None] = "67ec7c0706ec"
+down_revision: Union[str, None] = "7055cd620c33"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
