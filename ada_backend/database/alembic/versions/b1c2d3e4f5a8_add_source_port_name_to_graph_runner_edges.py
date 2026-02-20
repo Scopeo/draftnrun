c797ce792ad16e315a37c1e86804488e666fc8af
dianascopeo <diana@draftnrun.com>
2026-02-20 12:29:22 +0100

    fix alembic

--- ada_backend/database/alembic/versions/b1c2d3e4f5a8_add_source_port_name_to_graph_runner_edges.py
+++ ada_backend/database/alembic/versions/b1c2d3e4f5a8_add_source_port_name_to_graph_runner_edges.py
@@ -1,7 +1,7 @@
 """add source_port_name to graph_runner_edges
 
 Revision ID: b1c2d3e4f5a8
-Revises: a1b2c3d4e5f7
+Revises: a1b2c3d4e5g7
 Create Date: 2026-02-19 12:00:00.000000
 
 """
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "b1c2d3e4f5a8"
-down_revision: Union[str, None] = "a1b2c3d4e5f7"
+down_revision: Union[str, None] = "a1b2c3d4e5g7"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
