b785a6e7763ebd1e8f2f478cba363d60f0a44a85
Pablo Skewes <pablo.skewes@ug.uchile.cl>
2026-03-23 12:53:49 -0300

    fix migration

--- ada_backend/database/alembic/versions/a9c3d2e1f0b4_add_multiselect_ui_component_enum.py
+++ ada_backend/database/alembic/versions/a9c3d2e1f0b4_add_multiselect_ui_component_enum.py
@@ -1,7 +1,7 @@
 """add multiselect ui component enum value
 
 Revision ID: a9c3d2e1f0b4
-Revises: a3b4c5d6e7e8
+Revises: b1c2d3e4f5a7
 Create Date: 2026-03-17 00:00:00.000000
 
 """
@@ -12,7 +12,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "a9c3d2e1f0b4"
-down_revision: Union[str, None] = "a3b4c5d6e7e8"
+down_revision: Union[str, None] = "b1c2d3e4f5a7"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
