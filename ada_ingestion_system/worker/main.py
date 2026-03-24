9a1eae36e757aa689a83539c92b85f24a5afa6be
dianascopeo <diana@draftnrun.com>
2026-03-24 10:57:26 +0100

    change memory limit

--- ada_ingestion_system/worker/main.py
+++ ada_ingestion_system/worker/main.py
@@ -40,7 +40,7 @@ def _is_real_error(line: str) -> bool:
 # Redis configuration
 STREAM_NAME = os.getenv("REDIS_INGESTION_STREAM", "ada_ingestion_stream")
 MAX_CONCURRENT_INGESTIONS = int(os.getenv("MAX_CONCURRENT_INGESTIONS", 2))
-SUBPROCESS_MEMORY_LIMIT_MB = int(os.getenv("SUBPROCESS_MEMORY_LIMIT_MB", "400"))
+SUBPROCESS_MEMORY_LIMIT_MB = int(os.getenv("SUBPROCESS_MEMORY_LIMIT_MB", "1024"))
 
 
 def _set_memory_limit() -> None:
