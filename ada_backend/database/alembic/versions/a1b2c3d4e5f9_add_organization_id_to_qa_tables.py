b47a26fb13b3f6c0b20850293a50a55db47498b1
natacha-scopeo <natacha@draftnrun.com>
2026-03-27 17:43:03 +0100

    fix migration

--- ada_backend/database/alembic/versions/a1b2c3d4e5f9_add_organization_id_to_qa_tables.py
+++ ada_backend/database/alembic/versions/a1b2c3d4e5f9_add_organization_id_to_qa_tables.py
@@ -1,7 +1,7 @@
 """add organization_id to qa tables
 
-Revision ID: a1b2c3d4e5f8
-Revises: b2c3d4e5f6a7
+Revision ID: a1b2c3d4e5f9
+Revises: d3e4f5a6b7c8
 Create Date: 2026-02-09 10:00:00.000000
 
 """
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "a1b2c3d4e5f8"
-down_revision: Union[str, None] = "b2c3d4e5f6a7"
+revision: str = "a1b2c3d4e5f9"
+down_revision: Union[str, None] = "d3e4f5a6b7c8"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
