951f7e90d0fdcac060867b5e46ceed600b6925aa
dianascopeo <diana@draftnrun.com>
2026-04-09 09:51:11 +0200

    fix alembic

--- ada_backend/database/alembic/versions/e5f6a7b8c9d0_migrate_completion_model_to_field_expressions.py
+++ ada_backend/database/alembic/versions/e5f6a7b8c9d0_migrate_completion_model_to_field_expressions.py
@@ -6,7 +6,7 @@ Unmigrated components (Synthesizer, HybridSynthesizer, RelevantChunkSelector) ke
 completion_model as a BasicParameter.
 
 Revision ID: e5f6a7b8c9d0
-Revises: c5d6e7f8a9b0
+Revises: a1b2c3d4e5f9
 Create Date: 2026-03-24 00:00:00.000000
 
 """
@@ -18,7 +18,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = "e5f6a7b8c9d0"
-down_revision: Union[str, None] = "c5d6e7f8a9b0"
+down_revision: Union[str, None] = "a1b2c3d4e5f9"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 deploy_strategy: Union[str, None] = "breaking"
