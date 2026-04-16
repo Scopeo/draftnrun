7011ef5bb23acc6ef1316ba931f50bf63e1d12a6
dianascopeo <diana@draftnrun.com>
2026-04-16 12:24:52 +0200

    make parameters group for rag

--- ada_backend/database/seed/seed_rag_v4.py
+++ ada_backend/database/seed/seed_rag_v4.py
@@ -21,8 +21,21 @@ from ada_backend.database.seed.utils import (
     COMPONENT_VERSION_UUIDS,
     ParameterLLMConfig,
     build_completion_service_config_definitions,
+    build_components_parameters_assignments_to_parameter_groups,
+    build_parameters_group,
+    build_parameters_group_definitions,
 )
 from engine.components.synthesizer_prompts import get_base_synthetizer_prompt_template
+
+RAG_V4_PARAMETER_GROUP_UUIDS = {
+    "knowledge_parameters": UUID("f1a23b4c-5d6e-7f80-9a1b-2c3d4e5f6a7b"),
+    "advanced_knowledge_parameters": UUID("a2b34c5d-6e7f-8091-ab2c-3d4e5f6a7b8c"),
+    "llm_parameters": UUID("b3c45d6e-7f80-91a2-bc3d-4e5f6a7b8c9d"),
+    "advanced_llm_parameters": UUID("c4d56e7f-8091-a2b3-cd4e-5f6a7b8c9d0e"),
+    "reranker_parameters": UUID("d5e67f80-91a2-b3c4-de5f-6a7b8c9d0e1f"),
+    "vocabulary_search_parameters": UUID("e6f78091-a2b3-c4d5-ef6a-7b8c9d0e1f20"),
+    "formatter_parameters": UUID("f70891a2-b3c4-d5e6-f07b-8c9d0e1f2031"),
+}
 
 
 def seed_rag_v4_components(session: Session):
@@ -581,3 +594,180 @@ def seed_rag_v4_components(session: Session):
         release_stage=retriever_v2_version.release_stage,
         component_version_id=retriever_v2_version.id,
     )
+
+
+def seed_rag_v4_parameter_groups(session: Session):
+    parameter_groups = [
+        db.ParameterGroup(
+            id=RAG_V4_PARAMETER_GROUP_UUIDS["knowledge_parameters"],
+            name="Knowledge Parameters",
+        ),
+        db.ParameterGroup(
+            id=RAG_V4_PARAMETER_GROUP_UUIDS["advanced_knowledge_parameters"],
+            name="Advanced Knowledge Parameters",
+        ),
+        db.ParameterGroup(
+            id=RAG_V4_PARAMETER_GROUP_UUIDS["llm_parameters"],
+            name="LLM Parameters",
+        ),
+        db.ParameterGroup(
+            id=RAG_V4_PARAMETER_GROUP_UUIDS["advanced_llm_parameters"],
+            name="Advanced LLM Parameters",
+        ),
+        db.ParameterGroup(
+            id=RAG_V4_PARAMETER_GROUP_UUIDS["reranker_parameters"],
+            name="Reranker Parameters",
+        ),
+        db.ParameterGroup(
+            id=RAG_V4_PARAMETER_GROUP_UUIDS["vocabulary_search_parameters"],
+            name="Vocabulary Search Parameters",
+        ),
+        db.ParameterGroup(
+            id=RAG_V4_PARAMETER_GROUP_UUIDS["formatter_parameters"],
+            name="Formatter Parameters",
+        ),
+    ]
+    build_parameters_group(session, parameter_groups)
+
+    component_parameter_groups = [
+        db.ComponentParameterGroup(
+            component_version_id=COMPONENT_VERSION_UUIDS["rag_agent_v4"],
+            parameter_group_id=RAG_V4_PARAMETER_GROUP_UUIDS["knowledge_parameters"],
+            group_order_within_component=1,
+        ),
+        db.ComponentParameterGroup(
+            component_version_id=COMPONENT_VERSION_UUIDS["rag_agent_v4"],
+            parameter_group_id=RAG_V4_PARAMETER_GROUP_UUIDS["advanced_knowledge_parameters"],
+            group_order_within_component=2,
+        ),
+        db.ComponentParameterGroup(
+            component_version_id=COMPONENT_VERSION_UUIDS["rag_agent_v4"],
+            parameter_group_id=RAG_V4_PARAMETER_GROUP_UUIDS["llm_parameters"],
+            group_order_within_component=3,
+        ),
+        db.ComponentParameterGroup(
+            component_version_id=COMPONENT_VERSION_UUIDS["rag_agent_v4"],
+            parameter_group_id=RAG_V4_PARAMETER_GROUP_UUIDS["advanced_llm_parameters"],
+            group_order_within_component=4,
+        ),
+        db.ComponentParameterGroup(
+            component_version_id=COMPONENT_VERSION_UUIDS["rag_agent_v4"],
+            parameter_group_id=RAG_V4_PARAMETER_GROUP_UUIDS["reranker_parameters"],
+            group_order_within_component=5,
+        ),
+        db.ComponentParameterGroup(
+            component_version_id=COMPONENT_VERSION_UUIDS["rag_agent_v4"],
+            parameter_group_id=RAG_V4_PARAMETER_GROUP_UUIDS["vocabulary_search_parameters"],
+            group_order_within_component=6,
+        ),
+        db.ComponentParameterGroup(
+            component_version_id=COMPONENT_VERSION_UUIDS["rag_agent_v4"],
+            parameter_group_id=RAG_V4_PARAMETER_GROUP_UUIDS["formatter_parameters"],
+            group_order_within_component=7,
+        ),
+    ]
+    build_parameters_group_definitions(session, component_parameter_groups)
+
+    parameter_group_assignments = {
+        UUID("56f41bee-8473-415e-a1a5-6705b0af9fc0"): {  # data_source
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["knowledge_parameters"],
+            "parameter_order_within_group": 1,
+        },
+        UUID("84729d5d-4ee0-4f1e-abed-3b27283c8dc0"): {  # max_retrieved_chunks
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["knowledge_parameters"],
+            "parameter_order_within_group": 2,
+        },
+        UUID("452d5859-db93-43ce-a2b0-d072cd46e219"): {  # search_mode
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["knowledge_parameters"],
+            "parameter_order_within_group": 3,
+        },
+        UUID("4509b3b9-11df-4491-8441-e0c1bb23559b"): {  # enable_date_penalty_for_chunks
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["advanced_knowledge_parameters"],
+            "parameter_order_within_group": 1,
+        },
+        UUID("111d4a11-3722-4749-802e-dd1748dc050d"): {  # chunk_age_penalty_rate
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["advanced_knowledge_parameters"],
+            "parameter_order_within_group": 2,
+        },
+        UUID("fdc91df9-9e4b-44b3-be2a-c8ba236741cf"): {  # default_penalty_rate
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["advanced_knowledge_parameters"],
+            "parameter_order_within_group": 3,
+        },
+        UUID("66c18eb1-2653-465a-a435-8be8313f876c"): {  # metadata_date_key
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["advanced_knowledge_parameters"],
+            "parameter_order_within_group": 4,
+        },
+        UUID("8c9f7020-a65e-44da-a22e-0f6a19042dc0"): {  # max_retrieved_chunks_after_penalty
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["advanced_knowledge_parameters"],
+            "parameter_order_within_group": 5,
+        },
+        UUID("7af4208e-e8a0-46fc-87c7-34d3123afa11"): {  # completion_model
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["llm_parameters"],
+            "parameter_order_within_group": 1,
+        },
+        UUID("e446cd9c-56ac-4f91-8b52-ea6f4340ee25"): {  # prompt_template
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["llm_parameters"],
+            "parameter_order_within_group": 2,
+        },
+        UUID("b71a67b7-c634-4c02-b13e-707709fce859"): {  # temperature
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["advanced_llm_parameters"],
+            "parameter_order_within_group": 1,
+        },
+        UUID("01f50b86-bef8-462d-9a6d-1f993147e944"): {  # verbosity
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["advanced_llm_parameters"],
+            "parameter_order_within_group": 2,
+        },
+        UUID("7d035202-1639-4264-bab3-f5595748879d"): {  # reasoning
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["advanced_llm_parameters"],
+            "parameter_order_within_group": 3,
+        },
+        UUID("ad832572-d5f8-49f8-820a-5c9933aa04ab"): {  # api_key
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["advanced_llm_parameters"],
+            "parameter_order_within_group": 4,
+        },
+        UUID("bb2c46c1-7c19-4b77-a3ce-8d1cb9b504ba"): {  # use_reranker
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["reranker_parameters"],
+            "parameter_order_within_group": 1,
+        },
+        UUID("4aaa8fec-7a9a-4e04-a450-9d054699e7b7"): {  # cohere_model
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["reranker_parameters"],
+            "parameter_order_within_group": 2,
+        },
+        UUID("62788ddf-a40e-4e40-b332-47b689961805"): {  # score_threshold
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["reranker_parameters"],
+            "parameter_order_within_group": 3,
+        },
+        UUID("e3489939-50e6-48f4-8806-08e7761669f2"): {  # num_doc_reranked
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["reranker_parameters"],
+            "parameter_order_within_group": 4,
+        },
+        UUID("0bba7a67-c271-4558-b3ba-44ddbd536b0b"): {  # use_vocabulary_search
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["vocabulary_search_parameters"],
+            "parameter_order_within_group": 1,
+        },
+        UUID("7dd95b0c-f539-48b8-9c52-f96f32a781b9"): {  # vocabulary_context_data
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["vocabulary_search_parameters"],
+            "parameter_order_within_group": 2,
+        },
+        UUID("409e7c43-8d73-48d6-b66f-b0506af915e7"): {  # fuzzy_threshold
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["vocabulary_search_parameters"],
+            "parameter_order_within_group": 3,
+        },
+        UUID("22c922fb-b917-4c1c-9bd4-f8f838e37a49"): {  # fuzzy_matching_candidates
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["vocabulary_search_parameters"],
+            "parameter_order_within_group": 4,
+        },
+        UUID("bb5c1ec8-aafa-46e0-9d83-97af786eadda"): {  # vocabulary_context_prompt_key
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["vocabulary_search_parameters"],
+            "parameter_order_within_group": 5,
+        },
+        UUID("75959c9f-c748-4503-9f94-44fd23116260"): {  # use_formatter
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["formatter_parameters"],
+            "parameter_order_within_group": 1,
+        },
+        UUID("026c2ca1-f702-43f2-a530-e485a80dbd9c"): {  # add_sources
+            "parameter_group_id": RAG_V4_PARAMETER_GROUP_UUIDS["formatter_parameters"],
+            "parameter_order_within_group": 2,
+        },
+    }
+    build_components_parameters_assignments_to_parameter_groups(session, parameter_group_assignments)
