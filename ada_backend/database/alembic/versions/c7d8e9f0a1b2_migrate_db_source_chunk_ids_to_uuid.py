1101bf41d5087d13b4baf7834b0dc57bf0a4dbae
dianascopeo <diana@draftnrun.com>
2026-05-05 17:04:59 +0200

    fix alembic

--- ada_backend/database/alembic/versions/c7d8e9f0a1b2_migrate_db_source_chunk_ids_to_uuid.py
+++ ada_backend/database/alembic/versions/c7d8e9f0a1b2_migrate_db_source_chunk_ids_to_uuid.py
@@ -1,7 +1,7 @@
 """migrate_db_source_chunk_ids_to_uuid
 
 Revision ID: c7d8e9f0a1b2
-Revises: 88538199bc7b
+Revises: a3b4c5d6e7f9
 Create Date: 2026-04-27
 
 """
@@ -17,7 +17,7 @@ from sqlalchemy import create_engine, text
 from settings import settings
 
 revision: str = "c7d8e9f0a1b2"
-down_revision: Union[str, None] = "88538199bc7b"
+down_revision: Union[str, None] = "a3b4c5d6e7f9"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
